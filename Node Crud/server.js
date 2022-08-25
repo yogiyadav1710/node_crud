c039859d9576b7265f08d8164d12128f2d0598c3
D5_56586-yogesh <yogiyadav1710@gmail.com>
2022-08-25 18:55:10 +0530

    get api added

--- Node Crud/server.js
+++ Node Crud/server.js
@@ -1,6 +1,17 @@
 const express=require('express')
 var app=express()
 
+app.get('/',(req,res)=>{
+    // res.end("this is homepage,darling...")
+    res.send('this is homepage api..')
+});
+
+app.get('/usernames',(req,res)=>{
+
+    var usernames=['Aditya','Sahil','Sagar','Saurabh']
+    res.send(usernames)
+})
+
 app.listen(3000,function(){
     console.log('server started on port 3000..')
-})
+}) 
